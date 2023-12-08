ba841b7dee134ad2d5a1e38eecc3534c2e996d89
mdeghady <mostafadeghady22@gmail.com>
2023-12-08 11:28:57 +0200

    starting the project

--- IaC/variables.tf
+++ IaC/variables.tf
@@ -1,27 +1,27 @@
 variable "region" {
-    type = string
-    description = "Region for AWS Resources"
-    default = "us-east-1"
-  
+  type        = string
+  description = "Region for AWS Resources"
+  default     = "us-east-1"
+
 }
 
 variable "vpc_cidr" {
-    type = string
-    description = "Vpc cidr block"
-    default = "10.0.0.0/16"
-  
+  type        = string
+  description = "Vpc cidr block"
+  default     = "10.0.0.0/16"
+
 }
 
 variable "public_subnets" {
-    type = list(string)
-    description = "Vpc puplic subnets"
-    default = [ "10.0.1.0/24" ,  "10.0.2.0/24"]
-  
+  type        = list(string)
+  description = "Vpc puplic subnets"
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+
 }
 
 variable "private_subnets" {
-    type = list(string)
-    description = "Vpc private subnets"
-    default = [ "10.0.3.0/24" ,  "10.0.4.0/24"]
-  
+  type        = list(string)
+  description = "Vpc private subnets"
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+
 }
--- dags/myUtils/YoutubeRequest.py
+++ dags/myUtils/YoutubeRequest.py
@@ -3,16 +3,28 @@ from airflow.models import Variable
 import json
 import logging
 
+
+
 def getRequest(**context):
     api_key = Variable.get("api_key")
+
     # Variable returns countries_codes as a string so json.loads will return it to list
     countries_codes = json.loads(Variable.get("countries_codes"))
+
     for country_code in countries_codes:
         url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails%2C%20snippet%2C%20statistics&chart=mostPopular&regionCode={country_code}&key={api_key}"
         response = requests.request(method="GET",url=url)
 
-        context["task_instance"].xcom_push(key = country_code , value = response.json())
+        #retrieve the data from the body of the response as python dictionary
+        #response_data = response.text
+        response_data = response.json()
+        context["task_instance"].xcom_push(key=country_code + "_json",
+                                           value=response_data)
 
-        logging.info(f"The Used url is: {url}")
+        # #saving the json file
+        # with open(f"/data/{country_code}.json", "w") as outfile:
+        #     outfile.write(response_data)
+
+        logging.info(f"{country_code}.json saved to local disk")
 
 
