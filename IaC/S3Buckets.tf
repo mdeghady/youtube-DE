1d03dec8d9bf9b468e1cbad9396cd99ece031c49
mdeghady <mostafadeghady22@gmail.com>
2023-12-13 17:59:28 +0200

    adding mwaa bucket

--- IaC/S3Buckets.tf
+++ IaC/S3Buckets.tf
@@ -14,16 +14,48 @@ resource "aws_s3_bucket" "landing_data_bucket" {
   }
 }
 
-#s3 bucket for landing paquet data
+#s3 bucket for cleaned parquet data
 resource "aws_s3_bucket" "cleaned_data_bucket" {
   bucket        = "md-youtube-de-cleaned-data"
   force_destroy = true
+  tags          = var.project_tags
+}
 
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-    project     = "youtube-DE"
+##################################################################################
+# MWAA S3 Bucket
+##################################################################################
+
+#MWAA requires a bucket which is blocked for all public access and has versioning enabled
+resource "aws_s3_bucket" "mwaa_bucket" {
+  #It's private by default
+  bucket        = "md-mwaa"
+  force_destroy = true
+  tags          = var.project_tags
+}
+
+#add versioning to mwaa bucket
+resource "aws_s3_bucket_versioning" "mwaa_bucket_versioning" {
+  bucket = aws_s3_bucket.mwaa_bucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
+}
+
+#upload dags folder to MWAA bucket
+resource "aws_s3_object" "upload_dags_folder" {
+  for_each = fileset(var.mwaa_dags_folder, "**")
+  bucket   = aws_s3_bucket.mwaa_bucket.id
+  key      = "dags/${each.value}"
+  source   = "${var.mwaa_dags_folder}${each.value}"
+
+}
+
+#upload mwaa requirements file to MWAA bucket
+resource "aws_s3_object" "upload_requirements_file" {
+  bucket = aws_s3_bucket.mwaa_bucket.id
+  key    = "requirements.txt"
+  source = var.mwaa_requirements_file
+
 }
 
 
