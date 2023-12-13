1f7175dbb27efa7d2720f8e7395c872d557ec56c
mdeghady <mostafadeghady22@gmail.com>
2023-12-13 18:02:53 +0200

    adding mwaa variables

--- IaC/variables.tf
+++ IaC/variables.tf
@@ -15,13 +15,36 @@ variable "vpc_cidr" {
 variable "public_subnets" {
   type        = list(string)
   description = "Vpc puplic subnets"
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["10.0.2.0/24", "10.0.4.0/24", "10.0.6.0/24", "10.0.20.0/24"]
 
 }
 
 variable "private_subnets" {
   type        = list(string)
   description = "Vpc private subnets"
-  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+  default     = ["10.0.12.0/24", "10.0.14.0/24", "10.0.16.0/24", "10.0.18.0/24"]
 
+}
+
+variable "mwaa_dags_folder" {
+  type        = string
+  description = "dags folder path"
+  default     = "C:/Users/mosta/Desktop/youtube-DE/dags/"
+
+}
+
+variable "mwaa_requirements_file" {
+  type        = string
+  description = "requirement python libraries for mwaa"
+  default     = "C:/Users/mosta/Desktop/youtube-DE/requirements.txt"
+}
+
+variable "project_tags" {
+  type        = map(any)
+  description = "key value pairs of the project_tags"
+  default = {
+    Terraform   = "true"
+    Environment = "dev"
+    project     = "youtube-DE"
+  }
 }
